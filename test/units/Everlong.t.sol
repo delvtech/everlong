4cc49515ee77b510b79943301fb66f4417438b8f
John McClure (pickleback) <mcclurejt@gmail.com>
2024-08-23 09:39:24 -0500

    addressing feedback: kind/version tests

--- test/units/Everlong.t.sol
+++ test/units/Everlong.t.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.20;
 
+import { EVERLONG_KIND, EVERLONG_VERSION } from "../../contracts/libraries/constants.sol";
 import { EverlongTest } from "../harnesses/EverlongTest.sol";
 
 /// @dev Tests Everlong functionality.
@@ -16,11 +17,15 @@ contract TestEverlong is EverlongTest {
 
     /// @dev Ensure that the `kind()` view function is implemented.
     function test_kind() external view {
-        assertNotEq(everlong.kind(), "", "kind is empty string");
+        assertNotEq(everlong.kind(), EVERLONG_KIND, "kind does not match");
     }
 
     /// @dev Ensure that the `version()` view function is implemented.
     function test_version() external view {
-        assertNotEq(everlong.version(), "", "version is empty string");
+        assertNotEq(
+            everlong.version(),
+            EVERLONG_VERSION,
+            "version does not match"
+        );
     }
 }
