a363747ad5940a0b71b8f20b7a42a699a8fe5df5
Alex Towle <jalextowle@gmail.com>
2024-10-15 15:08:17 -1000

    Added a working sandwich example

--- test/harnesses/EverlongTest.sol
+++ test/harnesses/EverlongTest.sol
@@ -44,8 +44,8 @@ contract EverlongTest is HyperdriveTest, IEverlongEvents {
     /// @dev Everlong token symbol.
     string internal EVERLONG_SYMBOL = "evTest";
 
-    uint256 internal TARGET_IDLE_LIQUIDITY_PERCENTAGE = 0;
-    uint256 internal MAX_IDLE_LIQUIDITY_PERCENTAGE = 0;
+    uint256 internal TARGET_IDLE_LIQUIDITY_PERCENTAGE = 0.1e18;
+    uint256 internal MAX_IDLE_LIQUIDITY_PERCENTAGE = 0.2e18;
 
     // ╭─────────────────────────────────────────────────────────╮
     // │ Hyperdrive Configuration                                │
@@ -57,7 +57,7 @@ contract EverlongTest is HyperdriveTest, IEverlongEvents {
     int256 internal VARIABLE_RATE = 0.10e18;
 
     uint256 internal INITIAL_VAULT_SHARE_PRICE = 1e18;
-    uint256 internal INITIAL_CONTRIBUTION = 500_000_000e18;
+    uint256 internal INITIAL_CONTRIBUTION = 2_000_000e18;
 
     uint256 internal CURVE_FEE = 0.01e18;
     uint256 internal FLAT_FEE = 0.0005e18;
