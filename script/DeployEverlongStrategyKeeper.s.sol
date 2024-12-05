f531a72618585f10f24546ff116fce545f475cc0
John McClure (pickleback) <mcclurejt@gmail.com>
2024-12-04 18:52:21 -0600

    add deploy scripts for vaults and include shared base contract for artifact writing

--- script/DeployEverlongStrategyKeeper.s.sol
+++ script/DeployEverlongStrategyKeeper.s.sol
@@ -1,127 +1,26 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity 0.8.24;
 
-import "forge-std/Script.sol";
-import { IAccountant } from "../contracts/interfaces/IAccountant.sol";
-import { IRoleManager } from "../contracts/interfaces/IRoleManager.sol";
-import { IRoleManagerFactory } from "../contracts/interfaces/IRoleManagerFactory.sol";
-import { ROLE_MANAGER_FACTORY_ADDRESS, COMMON_REPORT_TRIGGER_ADDRESS } from "../contracts/libraries/Constants.sol";
+import { BaseDeployScript } from "./shared/BaseDeployScript.sol";
+import { COMMON_REPORT_TRIGGER_ADDRESS, EVERLONG_STRATEGY_KEEPER_KIND } from "../contracts/libraries/Constants.sol";
 import { EverlongStrategyKeeper } from "../contracts/EverlongStrategyKeeper.sol";
-import { ROLE_MANAGER_OUTPUT_DIR_NAME, DeployRoleManagerOutput } from "./DeployRoleManager.s.sol";
 
-string constant EVERLONG_KEEPER_CONTRACT_OUTPUT_DIR_NAME = "everlongKeeperContracts";
-
-struct DeployEverlongStrategyKeeperOutput {
-    address commonReportTrigger;
-    address deployer;
-    address keeper;
-    address keeperContract;
-    string keeperContractName;
-    address roleManager;
-    uint256 timestamp;
-}
-
-contract DeployEverlongStrategyKeeper is Script {
-    // ╭───────────────────────────────────────────────────────────────────────╮
-    // │                               Arguments                               │
-    // ╰───────────────────────────────────────────────────────────────────────╯
-
-    // ── Required ────────────────────────────────────────────────────────
-
+contract DeployEverlongStrategyKeeper is BaseDeployScript {
+    // Required Arguments
     uint256 internal DEPLOYER_PRIVATE_KEY;
     uint256 internal KEEPER_PRIVATE_KEY;
 
-    // ── Optional ────────────────────────────────────────────────────────
+    // Optional Arguments
+    string internal NAME;
+    string internal constant NAME_DEFAULT = "EVERLONG_STRATEGY_KEEPER";
 
-    // KEEPER_CONTRACT_NAME
+    string internal ROLE_MANAGER_PROJECT_NAME;
+    string internal ROLE_MANAGER_PROJECT_NAME_DEFAULT;
 
-    string internal KEEPER_CONTRACT_NAME;
-    string internal constant KEEPER_CONTRACT_NAME_DEFAULT =
-        "EVERLONG_STRATEGY_KEEPER";
+    // Artifact struct
+    KeeperContractArtifact internal output;
 
-    // ROLE_MANAGER_ADDRESS
-
-    address internal ROLE_MANAGER_ADDRESS;
-    address internal ROLE_MANAGER_ADDRESS_DEFAULT;
-
-    /// @dev Find the most-recently deployed RoleManager on the same chain and
-    ///      retrieve its address.
-    function resolveDefaultRoleManagerAddress() internal {
-        // Retrieve a list of all files and directories in the `DeployRoleManager`
-        // script output directory.
-        VmSafe.DirEntry[] memory entries = vm.readDir(
-            string.concat(
-                vm.projectRoot(),
-                "/deploy/",
-                vm.toString(block.chainid),
-                "/",
-                ROLE_MANAGER_OUTPUT_DIR_NAME
-            )
-        );
-        // Find the first path that isn't a directory, read the file contents,
-        // and extract the `RoleManager`'s address.
-        // Use the most-recently deployed `RoleManager`.
-        DeployRoleManagerOutput memory deployRoleManagerOutput;
-        for (uint256 i; i < entries.length; i++) {
-            if (vm.isFile(entries[i].path)) {
-                DeployRoleManagerOutput memory tmpOutput = abi.decode(
-                    vm.parseToml(vm.readFile(entries[i].path)),
-                    (DeployRoleManagerOutput)
-                );
-                if (tmpOutput.timestamp > deployRoleManagerOutput.timestamp) {
-                    deployRoleManagerOutput = tmpOutput;
-                }
-            }
-        }
-        ROLE_MANAGER_ADDRESS_DEFAULT = deployRoleManagerOutput.roleManager;
-    }
-
-    // ╭───────────────────────────────────────────────────────────────────────╮
-    // │                                Output                                 │
-    // ╰───────────────────────────────────────────────────────────────────────╯
-    DeployEverlongStrategyKeeperOutput internal output;
-
-    /// @dev Ensures that `/deploy/${CHAIN_ID}/keeperContracts` dir exists.
-    function validateOutputDir() internal {
-        string memory outputDir = string.concat(
-            vm.projectRoot(),
-            "/deploy/",
-            vm.toString(block.chainid),
-            "/",
-            EVERLONG_KEEPER_CONTRACT_OUTPUT_DIR_NAME
-        );
-        require(
-            vm.isDir(outputDir),
-            string.concat("ERROR: output dir '", outputDir, "' does not exist.")
-        );
-    }
-
-    /// @dev `/deploy/${CHAIN_ID}/everlongKeeperContracts/${KEEPER_CONTRACT_NAME}.toml`
-    function getOutputFilePath(
-        string memory name
-    ) internal view returns (string memory path) {
-        path = string.concat(
-            vm.projectRoot(),
-            "/deploy/",
-            vm.toString(block.chainid),
-            "/",
-            EVERLONG_KEEPER_CONTRACT_OUTPUT_DIR_NAME,
-            "/",
-            name,
-            ".toml"
-        );
-    }
-
-    // ╭───────────────────────────────────────────────────────────────────────╮
-    // │                                  Run                                  │
-    // ╰───────────────────────────────────────────────────────────────────────╯
-
-    /// @dev Deploys a RoleManager and configures the default vault fee values
-    ///      for the Accountant.
     function run() external {
-        // Ensure the output directory exists.
-        validateOutputDir();
-
         // Read required arguments.
         DEPLOYER_PRIVATE_KEY = vm.envUint("DEPLOYER_PRIVATE_KEY");
         output.deployer = vm.addr(DEPLOYER_PRIVATE_KEY);
@@ -131,45 +30,65 @@ contract DeployEverlongStrategyKeeper is Script {
         // Validate required arguments.
         require(
             output.deployer != output.keeper,
-            "deployer and keeper accounts must be different"
+            "Error: deployer and keeper accounts must be different"
         );
 
         // Resolve optional argument defaults.
-        resolveDefaultRoleManagerAddress();
+        ROLE_MANAGER_PROJECT_NAME_DEFAULT = hasDefaultRoleManagerArtifact()
+            ? getDefaultRoleManagerArtifact().projectName
+            : "";
 
         // Read optional arguments.
-        KEEPER_CONTRACT_NAME = vm.envOr(
-            "KEEPER_CONTRACT_NAME",
-            KEEPER_CONTRACT_NAME_DEFAULT
+        NAME = vm.envOr("NAME", NAME_DEFAULT);
+        output.name = NAME;
+        ROLE_MANAGER_PROJECT_NAME = vm.envOr(
+            "ROLE_MANAGER_PROJECT_NAME",
+            ROLE_MANAGER_PROJECT_NAME_DEFAULT
         );
-        output.keeperContractName = KEEPER_CONTRACT_NAME;
-        ROLE_MANAGER_ADDRESS = vm.envOr(
-            "ROLE_MANAGER_ADDRESS",
-            ROLE_MANAGER_ADDRESS_DEFAULT
-        );
-        output.roleManager = ROLE_MANAGER_ADDRESS;
+        output.roleManagerProjectName = ROLE_MANAGER_PROJECT_NAME;
 
         // Validate optional arguments.
         require(
-            ROLE_MANAGER_ADDRESS != address(0),
-            "ROLE_MANAGER_ADDRESS cannot be the zero address"
+            vm.isFile(
+                getRoleManagerArtifactPath(output.roleManagerProjectName)
+            ),
+            "ERROR: ROLE_MANAGER_PROJECT_NAME cannot be found in artifacts"
         );
+        address roleManagerAddress = getRoleManagerArtifact(
+            output.roleManagerProjectName
+        ).roleManager;
+
+        // Include the KeeperContract's `kind` in the artifact.
+        output.kind = EVERLONG_STRATEGY_KEEPER_KIND;
+
+        // Ensure output directory exists.
         require(
-            !vm.isFile(getOutputFilePath(KEEPER_CONTRACT_NAME)),
+            vm.isDir(getKeeperContractArtifactDir()),
             string.concat(
-                "ERROR: An EverlongStrategyKeeper with name '",
-                KEEPER_CONTRACT_NAME,
-                "' already exists."
+                "Error: KeeperContract artifact directory '",
+                getKeeperContractArtifactDir(),
+                "' has not been created"
             )
         );
 
-        // Deploy the EverlongStrategyKeeper contract.
-        // Transfer ownership to the `keeper` address.
+        // Ensure output file does not exist.
+        require(
+            !vm.isFile(getKeeperContractArtifactPath(output.name)),
+            string.concat(
+                "Error: KeeperContract artifact file '",
+                getKeeperContractArtifactPath(output.name),
+                "' already exists"
+            )
+        );
+
+        // As the `deployer` account:
+        //   1. Deploy the KeeperContract.
+        //   2. Transfer ownership to the `keeper` account.
         vm.startBroadcast(DEPLOYER_PRIVATE_KEY);
         output.keeperContract = address(
             new EverlongStrategyKeeper(
-                output.keeperContractName,
-                output.roleManager,
+                output.name,
+                roleManagerAddress,
                 COMMON_REPORT_TRIGGER_ADDRESS
             )
         );
@@ -177,36 +96,8 @@ contract DeployEverlongStrategyKeeper is Script {
             output.keeper
         );
         vm.stopBroadcast();
-        output.commonReportTrigger = COMMON_REPORT_TRIGGER_ADDRESS;
 
         // Write output in toml format to a file.
-        string memory outputToml = "output";
-        vm.serializeAddress(
-            outputToml,
-            "commonReportTrigger",
-            output.commonReportTrigger
-        );
-        vm.serializeAddress(outputToml, "deployer", output.deployer);
-        vm.serializeAddress(outputToml, "keeper", output.keeper);
-        vm.serializeAddress(
-            outputToml,
-            "keeperContract",
-            output.keeperContract
-        );
-        vm.serializeString(
-            outputToml,
-            "keeperContractName",
-            output.keeperContractName
-        );
-        vm.serializeAddress(outputToml, "roleManager", output.roleManager);
-        string memory finalOutputToml = vm.serializeUint(
-            outputToml,
-            "timestamp",
-            block.timestamp
-        );
-        vm.writeToml(
-            finalOutputToml,
-            getOutputFilePath(output.keeperContractName)
-        );
+        writeKeeperContractArtifact(output);
     }
 }
